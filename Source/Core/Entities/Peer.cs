10c31e6f155da7917d9dd39eaf4ffe74eefd616d
Drake Lambert <drake.askme2@gmail.com>
2018-10-24 14:39:49 -0500

    rename User to Peer

--- Source/Core/Entities/Peer.cs
+++ Source/Core/Entities/Peer.cs
@@ -4,23 +4,23 @@ using Ardalis.GuardClauses;
 namespace DrakeLambert.Peerra.Core.Entities
 {
     /// <summary>
-    /// A user of the Peerra system.
+    /// A user of the Peerra system, referred to as a Peer.
     /// </summary>
-    public class User
+    public class Peer
     {
         /// <summary>
-        /// The unique identifier of the user.
+        /// The unique identifier of the peer.
         /// </summary>
         public string Id { get; }
 
         /// <summary>
-        /// Constructs a new user.
+        /// Constructs a new peer.
         /// </summary>
-        /// <param name="id">The id of the user. Must not be null.</param>
+        /// <param name="id">The id of the peer. Must not be null.</param>
         /// <exception cref="ArgumentNullException">
         /// Thrown when id is null.
         /// </exception>
-        public User(string id)
+        public Peer(string id)
         {
             Guard.Against.Null(id, nameof(id));
             Id = id;

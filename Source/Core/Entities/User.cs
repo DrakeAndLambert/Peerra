e6a65a3634dfb73a5da9cd6133ce0606e5be54f9
Drake Lambert <drake.askme2@gmail.com>
2018-10-24 13:08:20 -0500

    added user entity

---
+++ Source/Core/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ardalis.GuardClauses;
+
+namespace DrakeLambert.Peerra.Core.Entities
+{
+    /// <summary>
+    /// A user of the Peerra system.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// The unique identifier of the user.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Constructs a new user.
+        /// </summary>
+        /// <param name="id">The id of the user. Must not be null.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when id is null.
+        /// </exception>
+        public User(string id)
+        {
+            Guard.Against.Null(id, nameof(id));
+            Id = id;
+        }
+    }
+}

f9aa54085c69c367998d4948b773c0c8dd84c5cc
Drake Lambert <drake.askme2@gmail.com>
2018-10-29 15:36:30 -0500

    added db context

---
+++ Source/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DrakeLambert.Peerra.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrakeLambert.Peerra.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Peer> Peers { get; set; }
+        
+        public DbSet<Connection> Connections { get; set; }
+        
+        public DbSet<ConnectionRequest> ConnectionRequests { get; set; }
+        
+        public DbSet<Skill> Skills { get; set; }
+
+        
+    }
+}

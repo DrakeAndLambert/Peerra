a9be972e261a341e75d7676d9b290f3c25dd7e98
Drake Lambert <drake.askme2@gmail.com>
2018-11-01 15:47:18 -0500

    added new base projects

---
+++ Source/WebApi/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DrakeLambert.Peerra.WebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}

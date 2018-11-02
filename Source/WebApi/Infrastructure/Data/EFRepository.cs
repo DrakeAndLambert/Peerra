9a26340065e4bdedb9afd67c4c3a0e14a31e54a0
Drake Lambert <drake.askme2@gmail.com>
2018-11-02 15:00:59 -0500

    added EFRepository

---
+++ Source/WebApi/Infrastructure/Data/EFRepository.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DrakeLambert.Peerra.WebApi.Core.Data;
+using DrakeLambert.Peerra.WebApi.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrakeLambert.Peerra.WebApi.Infrastructure.Data
+{
+    public class EFRepository<T, TKey> : IAsyncRepository<T, TKey> where T : class, IEntity<TKey>
+    {
+        private readonly AppDbContext _context;
+
+        public EFRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> GetByIdAsync(TKey id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}

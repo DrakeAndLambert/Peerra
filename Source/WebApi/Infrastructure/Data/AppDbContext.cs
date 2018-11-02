a05d47e783416fd794f3ef5900a8e6de1991819d
Drake Lambert <drake.askme2@gmail.com>
2018-11-01 21:34:47 -0500

    removed db sets temporarily

--- Source/WebApi/Infrastructure/Data/AppDbContext.cs
+++ Source/WebApi/Infrastructure/Data/AppDbContext.cs
@@ -5,15 +5,20 @@ namespace DrakeLambert.Peerra.WebApi.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Connection> Connections { get; set; }
+        // public DbSet<Connection> Connections { get; set; }
 
-        public DbSet<ConnectionRequest> ConnectionRequests { get; set; }
+        // public DbSet<ConnectionRequest> ConnectionRequests { get; set; }
 
         public DbSet<Peer> Peers { get; set; }
 
-        public DbSet<Skill> Skills { get; set; }
+        // public DbSet<Skill> Skills { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            // TODO: Configure models
+        }
     }
 }

d168cdad2e1ddbe089b2f4b1b4dcb1a72e0c1068
Drake Lambert <drake.askme2@gmail.com>
2018-11-06 12:30:39 -0600

    making Peer and IdentityUser

--- Source/WebApi/Core/Entities/Peer.cs
+++ Source/WebApi/Core/Entities/Peer.cs
@@ -1,20 +1,11 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace DrakeLambert.Peerra.WebApi.Core.Entities
 {
     /// <summary>
     /// A user of the Peerra system, referred to as a Peer.
     /// </summary>
-    public class Peer : IEntity<Guid>
-    {
-        /// <summary>
-        /// The unique identifier of the peer.
-        /// </summary>
-        public Guid Id { get; private set; }
-
-        public Peer(Guid id)
-        {
-            Id = id;
-        }
-    }
+    public class Peer : IdentityUser<Guid>, IEntity<Guid>
+    { }
 }

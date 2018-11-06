2e98347596e6f26852ffe3d959e771c7db8ea801
Drake Lambert <drake.askme2@gmail.com>
2018-11-06 12:45:14 -0600

    add DeleteUser

---
+++ Source/WebApi/Infrastructure/UseCases/DeleteUser.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using DrakeLambert.Peerra.WebApi.Core.Entities;
+using DrakeLambert.Peerra.WebApi.Core.UseCases;
+using Microsoft.AspNetCore.Identity;
+
+namespace DrakeLambert.Peerra.WebApi.Infrastructure.UseCases
+{
+    public class DeleteUser : IDeleteUser
+    {
+        private readonly UserManager<Peer> _userManager;
+
+        public DeleteUser(UserManager<Peer> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task HandleAsync(string username, string password)
+        {
+            Guard.Against.Null(username, nameof(username));
+            Guard.Against.Null(password, nameof(password));
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                await _userManager.DeleteAsync(user);
+            }
+        }
+    }
+}

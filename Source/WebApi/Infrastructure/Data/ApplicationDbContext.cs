ea289227986dbadbfb53fce2d2d6051988096ec4
Drake Lambert <drake.askme2@gmail.com>
2018-11-07 10:21:50 -0600

    Removing roles from app

--- Source/WebApi/Infrastructure/Data/ApplicationDbContext.cs
+++ Source/WebApi/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using DrakeLambert.Peerra.WebApi.Core.Entities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DrakeLambert.Peerra.WebApi.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, Guid>
+    public class ApplicationDbContext : IdentityUserContext<ApplicationUser, Guid>
     {
         // public DbSet<Connection> Connections { get; set; }
 
         // public DbSet<ConnectionRequest> ConnectionRequests { get; set; }
-
-        public DbSet<ApplicationUser> Peers { get; set; }
 
         // public DbSet<Skill> Skills { get; set; }
 
@@ -20,8 +17,6 @@ namespace DrakeLambert.Peerra.WebApi.Infrastructure.Data
         { }
 
         protected override void OnModelCreating(ModelBuilder model)
-        {
-            model.Entity<ApplicationUser>().HasKey(peer => peer.)
-        }
+        { }
     }
 }

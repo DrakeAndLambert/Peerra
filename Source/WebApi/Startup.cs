39c974a8083dc80b9593511e722566e9d3f64df5
Drake Lambert <drake.askme2@gmail.com>
2018-11-12 17:27:04 -0600

    configuring auth in startup

--- Source/WebApi/Startup.cs
+++ Source/WebApi/Startup.cs
@@ -1,21 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using DrakeLambert.Peerra.WebApi.Core.Data;
-using DrakeLambert.Peerra.WebApi.Core.Entities;
 using DrakeLambert.Peerra.WebApi.Infrastructure.Data;
+using DrakeLambert.Peerra.WebApi.WebCore.Authentication;
+using DrakeLambert.Peerra.WebApi.WebCore.Authentication.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace DrakeLambert.Peerra.WebApi
 {
     public class Startup
     {
+        /// <summary>
+        /// Creates a new instance with the given configuration.
+        /// </summary>
+        /// <param name="configuration">The configuration for the web host.</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// The configuration for the web host.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -25,7 +43,7 @@ namespace DrakeLambert.Peerra.WebApi
                 options.UseInMemoryDatabase(nameof(ApplicationDbContext));
             });
 
-            services.AddIdentityCore<ApplicationUser>(options =>
+            services.AddIdentityCore<WebUser>(options =>
             {
                 options.Password.RequireDigit = false;
                 options.Password.RequiredLength = 6;
@@ -43,18 +61,38 @@ namespace DrakeLambert.Peerra.WebApi
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 options.IncludeXmlComments(xmlPath);
+
+                options.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    In = "header",
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = "apiKey"
+                });
+
+                options.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    { "Bearer", new string[] { } }
+                });
             });
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseInMemoryDatabase(nameof(ApplicationDbContext));
-            });
+            var authOptions = Configuration.GetSection(nameof(AuthOptions));
 
-            services.AddTransient(typeof(IAsyncRepository<,>), typeof(EFRepository<,>));
+            services.Configure<AuthOptions>(authOptions);
 
-            services.AddAuthentication().AddJwtBearer(options =>
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
-                
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateActor = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = authOptions.Get<AuthOptions>().SecurityKey
+                };
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
             });
         }
 
@@ -74,6 +112,8 @@ namespace DrakeLambert.Peerra.WebApi
             });
 
             app.UseSwagger();
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }

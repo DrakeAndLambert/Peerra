d78c92f51d661ca6eba0d9fe993c24f4ff222501
Drake Lambert <drake.askme2@gmail.com>
2018-11-12 17:26:45 -0600

    using new webuser in app db context

--- Source/WebApi/Infrastructure/Data/ApplicationDbContext.cs
+++ Source/WebApi/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,33 +1,17 @@
-﻿using System;
-using DrakeLambert.Peerra.WebApi.Core.Entities;
+﻿using DrakeLambert.Peerra.WebApi.WebCore.Authentication.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DrakeLambert.Peerra.WebApi.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityUserContext<ApplicationUser, Guid>
+    public class ApplicationDbContext : IdentityUserContext<WebUser>
     {
-        public DbSet<Connection> Connections { get; set; }
-
-        public DbSet<Connection> ConnectionRequests { get; set; }
-
-        public DbSet<Skill> Skills { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         { }
 
         protected override void OnModelCreating(ModelBuilder model)
         {
-            model.Entity<Connection>(connection =>
-            {
-                connection.HasKey(e => new { Mentor = e.Mentor.Id, Mentoree = e.Mentoree.Id });
-
-                connection.HasOne(e => e.Mentor).WithMany();
-
-                connection.HasOne(e => e.Mentoree).WithMany();
-
-                connection.HasMany(e => e.CommonSkills);
-            });
+            base.OnModelCreating(model);
         }
     }
 }

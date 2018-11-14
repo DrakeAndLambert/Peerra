a69e7266411e3b7445829341bd60047af9ad8afb
Drake Lambert <drake.askme2@gmail.com>
2018-11-14 14:44:28 -0600

    added annotations to new user dto

--- Source/WebApi2/Web/Dto/NewUserDto.cs
+++ Source/WebApi2/Web/Dto/NewUserDto.cs
@@ -1,13 +1,18 @@
-﻿namespace DrakeLambert.Peerra.WebApi2.Web.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrakeLambert.Peerra.WebApi2.Web.Dto
 {
     public class NewUserDto
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
         public string Bio { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }

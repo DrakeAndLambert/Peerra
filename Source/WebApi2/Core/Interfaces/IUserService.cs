f7d465bb631e2d37eb98b38d9feef0681ea8f75a
Drake Lambert <drake.askme2@gmail.com>
2018-11-14 14:45:33 -0600

    renamed methods in IUserService

--- Source/WebApi2/Core/Interfaces/IUserService.cs
+++ Source/WebApi2/Core/Interfaces/IUserService.cs
@@ -7,10 +7,10 @@ namespace DrakeLambert.Peerra.WebApi2.Core.Interfaces
 {
     public interface IUserService
     {
-        Task<Result<User>> AddUser(User user);
+        Task<Result<User>> AddUserAsync(User user);
 
         Task<Result<User>> GetUserAsync(string username);
 
-        Task<Result> DeleteUser(Guid id);
+        Task<Result> DeleteUserAsync(Guid id);
     }
 }

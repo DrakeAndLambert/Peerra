0bd8ece095835dbb3219f149acde83f18f300ecf
DrakeLambert <drake.askme2@gmail.com>
2018-12-08 16:29:16 -0600

    using ApplicationUser

--- Source/Source/Data/ApplicationDbContext.cs
+++ Source/Source/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DrakeLambert.Peerra.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DrakeLambert.Peerra.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- Source/Source/Startup.cs
+++ Source/Source/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using DrakeLambert.Peerra.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using DrakeLambert.Peerra.Entities;
 
 namespace DrakeLambert.Peerra
 {
@@ -38,7 +39,7 @@ namespace DrakeLambert.Peerra
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 

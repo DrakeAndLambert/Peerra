e0c9eb6425e0eba9d60032d55f977b2358abd853
Drake Lambert <drake.askme2@gmail.com>
2018-12-14 11:08:27 -0600

    reorganized project

--- Source/WebApi/IdentityCore/Interfaces/ITokenValidator.cs
+++
@@ -1,9 +0,0 @@
-using DrakeLambert.Peerra.WebApi.SharedKernel.Dto;
-
-namespace DrakeLambert.Peerra.WebApi.IdentityCore.Interfaces
-{
-    public interface ITokenValidator
-    {
-        Result<string> ValidateAndGetUser(string accessToken);
-    }
-}
--- Source/WebApi/Web/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace DrakeLambert.Peerra.WebApi.Web.Entities
-{
-    public class User : IdentityUser
-    {
-        public string Bio { get; set; }
-    }
-}

5620f1e992c44d0da452ac7190b6dacf498240bb
Drake Lambert <drake.askme2@gmail.com>
2018-12-17 21:44:43 -0600

    added view raw json tree feature

--- Source/Pages/IssueTree.cshtml.cs
+++ Source/Pages/IssueTree.cshtml.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using DrakeLambert.Peerra.Data;
 using DrakeLambert.Peerra.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -23,7 +24,7 @@ namespace DrakeLambert.Peerra.Pages
 
         public string JsonIssues { get; set; }
 
-        public async Task OnGet()
+        public async Task<IActionResult> OnGet([FromQuery] bool raw = false)
         {
             var issues = (await _context.Issues.ToListAsync()).Where(issue => issue.ParentId == Guid.Empty);
 
@@ -35,6 +36,15 @@ namespace DrakeLambert.Peerra.Pages
             serializerSettings.ContractResolver = jsonResolver;
 
             JsonIssues = JsonConvert.SerializeObject(issues, Formatting.Indented, serializerSettings);
+
+            if (raw)
+            {
+                return new OkObjectResult(JsonIssues);
+            }
+            else
+            {
+                return Page();
+            }
         }
 
         private class PropertyIgnoreContractResolver : DefaultContractResolver

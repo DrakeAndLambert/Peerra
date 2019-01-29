2cc40bdafcae8eb896084f8b8d70ba96b1d04348
Drake Lambert <drake-lambert@outlook.com>
2019-01-29 11:13:12 -0600

    more controls on peer issue page

--- Source/Pages/Issues/Peer.cshtml.cs
+++ Source/Pages/Issues/Peer.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using DrakeLambert.Peerra.Data;
 using DrakeLambert.Peerra.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -16,13 +18,15 @@ namespace DrakeLambert.Peerra.Pages.Issues
 
 		public Issue Issue { get; set; }
 
+		public HelpRequest UserHelpRequest { get; set; }
+
 		public PeerModel(ApplicationDbContext context, SignInManager<ApplicationUser> signInManager)
 		{
 			_context = context;
 			_signInManager = signInManager;
 		}
 
-		public async System.Threading.Tasks.Task<IActionResult> OnGetAsync(Guid id)
+		public async Task<IActionResult> OnGetAsync([FromRoute] Guid id)
 		{
 			Issue = await _context.Issues
 				.Include(i => i.HelpRequests)
@@ -43,8 +47,52 @@ namespace DrakeLambert.Peerra.Pages.Issues
 				{
 					return RedirectToPage("/Issues/Single", new { id = id });
 				}
+
+				UserHelpRequest = Issue.HelpRequests.SingleOrDefault(hr => hr.HelperId == user.Id);
 			}
-			
+
+			return Page();
+		}
+
+		[Authorize]
+		public async Task<IActionResult> OnPostDeclineAsync([FromRoute] Guid id)
+		{
+			Issue = await _context.Issues
+							.Include(i => i.HelpRequests)
+								.ThenInclude(hr => hr.Helper)
+							.Include(i => i.Owner)
+							.Include(i => i.Topic)
+							.SingleOrDefaultAsync(i => i.Id == id);
+
+			if (Issue == null)
+			{
+				return RedirectToPage("/NotFound", new { message = "This issue may have been removed or moved." });
+			}
+
+			var user = await _signInManager.UserManager.GetUserAsync(User);
+
+			if (user.Id == Issue.OwnerId)
+			{
+				return RedirectToPage("/Issues/Single", new { id = id });
+			}
+
+			UserHelpRequest = Issue.HelpRequests.SingleOrDefault(hr => hr.HelperId == user.Id);
+
+			if (UserHelpRequest == null)
+			{
+				UserHelpRequest = new HelpRequest
+				{
+					Helper = user,
+					Issue = Issue
+				};
+
+				Issue.HelpRequests.Add(UserHelpRequest);
+			}
+
+			UserHelpRequest.Status = HelpRequestStatus.Declined;
+
+			await _context.SaveChangesAsync();
+
 			return Page();
 		}
 	}

c4bd1f1f799b35a1f9dbd25dc60a02a73e596cbd
Drake Lambert <drake-lambert@outlook.com>
2019-01-29 08:40:40 -0600

    added custom registration

---
+++ Source/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using DrakeLambert.Peerra.Data;
+using DrakeLambert.Peerra.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Peerra.Areas.Identity.IdentityHostingStartup))]
+namespace Peerra.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

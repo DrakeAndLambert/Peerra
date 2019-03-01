7ec7c8fe4094fd4954ebc9e88b1d75774d63cc18
DrakeLambert <drake.askme2@gmail.com>
2019-03-01 15:34:26 -0600

    added topic knowledge

--- Source/Controllers/TopicKnowledgeController.cs
+++ Source/Controllers/TopicKnowledgeController.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using System.Threading.Tasks;
 using DrakeLambert.Peerra.Data;
+using DrakeLambert.Peerra.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,10 +16,12 @@ namespace DrakeLambert.Peerra.Controllers
     public class TopicKnowledgeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public TopicKnowledgeController(ApplicationDbContext context)
+        public TopicKnowledgeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [HttpGet("rootTopics")]
@@ -30,14 +34,69 @@ namespace DrakeLambert.Peerra.Controllers
             return Ok(rootTopics);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetChildTopicsAsync(Guid id)
+        [HttpGet("topics/{id}")]
+        public async Task<IActionResult> GetChildTopicsAsync([FromRoute] Guid id)
         {
             var childTopics = await _context.Topics.Where(topic => topic.ParentId == id)
                 .Select(topic => new { Title = topic.Title, Description = topic.Description, Id = topic.Id })
                 .ToListAsync();
 
             return Ok(childTopics);
+        }
+
+        [HttpPost("register/{id}")]
+        public async Task<IActionResult> RegisterKnowledge([FromRoute] Guid id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var alreadyHasKnowledge = await _context.UserTopics.AnyAsync(userTopic => userTopic.TopicId == id && userTopic.UserId == user.Id);
+
+            if (!alreadyHasKnowledge)
+            {
+                var newUserTopic = new UserTopic { TopicId = id, UserId = user.Id };
+                _context.UserTopics.Add(newUserTopic);
+                await _context.SaveChangesAsync();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost("unregister/{id}")]
+        public async Task<IActionResult> UnregisterKnowledge([FromRoute] Guid id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var existingKnowledge = await _context.UserTopics.Where(userTopic => userTopic.TopicId == id && userTopic.UserId == user.Id).ToListAsync();
+
+            if (existingKnowledge.Count > 0)
+            {
+                _context.UserTopics.RemoveRange(existingKnowledge);
+                await _context.SaveChangesAsync();
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("has/{id}")]
+        public async Task<IActionResult> HasKnowledge([FromRoute] Guid id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var alreadyHasKnowledge = await _context.UserTopics.AnyAsync(userTopic => userTopic.TopicId == id && userTopic.UserId == user.Id);
+
+            return Ok(alreadyHasKnowledge);
         }
     }
 }
---
+++ Source/Pages/Topics/Knowledge.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DrakeLambert.Peerra.Pages.Topics
+{
+    [Authorize]
+    public class KnowledgeModel : PageModel
+    { }
+}
